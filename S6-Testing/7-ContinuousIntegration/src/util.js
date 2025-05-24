0a34bcc0977ad188d49d74306abc20c9469f9bd5
Jay Chakra <jaychakra.jc@gmail.com>
2025-05-24 11:25:43 +0530

    Added CI

---
+++ S6-Testing/7-ContinuousIntegration/src/util.js
@@ -0,0 +1,10 @@
+function processNumber(a, b) {
+    let result = a + b; 
+    if (result > 10) { 
+        result = result * 2; return result;
+    } else {  
+        result = (a * b) +
+          (a - b);
+    }
+  return result; 
+}
